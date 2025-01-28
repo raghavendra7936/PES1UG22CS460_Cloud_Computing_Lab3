e9da61b7ab38e87eeb87c722050b697cc4848b24
raghavendra7936 <118116240+raghavendra7936@users.noreply.github.com>
2025-01-28 20:38:44 +0530

    Update __init__ (1).py

--- __init__ (1).py
+++ __init__ (1).py
@@ -1,29 +1,2 @@
-from products import dao
-
-
-class Product:
-    def _init_(self, id: int, name: str, description: str, cost: float, qty: int = 0):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.cost = cost
-        self.qty = qty
-
-    def load(data):
-        return Product(data['id'], data['name'], data['description'], data['cost'], data['qty'])
-
-
-def list_products() -> list[dict]:
-    products = dao.list_products()
-    return [{"id": p["id"], "name": p["name"], "description": p["description"], "cost": p["cost"]} for p in products]
-
-
-
-def get_product(product_id: int) -> Product:
-    return Product.load(dao.get_product(product_id))
-
-
-def add_product(product: dict):
-    dao.add_product(product)
 
 
